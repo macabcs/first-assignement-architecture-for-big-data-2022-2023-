fe0e6761255704007344bb4dcde26a8e91c76bc6
alessio <alessio.bonatesta@studenti.unimi.it>
2022-11-03 13:51:33 +0100

    sboh

--- main.py
+++ main.py
@@ -1,64 +1,78 @@
 from abc import abstractmethod
-from datetime import datetime
-from turtle import update
 from xmlrpc.client import boolean
 from sql_conn import Sql_conn
 import threading
 import time
+
 class query:
-    def __init__(self, listColumn, listTables, listCondition) -> None:
+    def __init__(self, listTables, listColumns, listCondition) -> None:
         pass
     def checkInjection(self) -> boolean:
         pass
 
-class Data:  #format to represent data read from OneStream and to be written on the hist databases
+class Data:  #class used to interpret and represent data read from OneStream and to write on the hist databases
     pass
+
 class Database:
     def __init__(self,address,port,user,password) -> None:
         self.address=address
         self.port=port
         self.user=user
         self.password=password
-        pass
+
+
 class BatchSqlExtractor:
 
-    def __init__(self,dataSince,oneStreamDb: Database, histDb: Database, query : query) -> None: #add paramse for the secondo db, create second database in attribute
+    def __init__(self,oneStreamDb: Database, histDb: Database, query : query) -> None: #add paramse for the secondo db, create second database in attribute
         self.oneStreamConnection= self.__connect(oneStreamDb) #control update time
         self.histDBConnection = self.__connect(histDb)
-        self.lastUpdate = dataSince
+        self.lastUpdate = 0
         self.queryParams = query
 
         x = threading.Thread(target= self.__synchronizer)
         x.start()
 
-    def __synchronizer(self): #implement using update method 
+    def __synchronizer(self):
         while(True):
-            time.sleep(60*60*24*1000)
             while(self.__updateCondtion()):
-                update()
+                self.update()
+            time.sleep(60*60*24*1000)
+
+    def update(self):
+        listOfQuerys =self.readLog()    # retrieve executed query
+        filteredQuerys = self.filterQuery(listOfQuerys)     # remove select query
+        refactoredQuerys = self.refactorQuery(filteredQuerys)      # adding timestamp value 
+        
+        for query in refactoredQuerys:  
+             self.histDBConnection.execute_query(query)     #execute every new query (insert, update and delete)
+        
+        self.lastUpdate = time.time()   # update lastUpdate attribute to actual timestamp 
 
 
-    
-    def __updateCondition(self) -> bool:  #implement the policy update based on the Month End Closing activities.
+    @abstractmethod
+    def readLog(self): 
+        #define this method such that it makes a SELECT query to the log table of the original database
+        #and return a list of string pair ( query , timestamp ) 
+        #this method has to select rows that have timestamp >= self.lastUpdate (initially zero because we want to take all data at the beginning)
         pass
 
-    def __update(self) -> None: #dabase
-        updateTo = datetime.today().replace(day=1).timestamp()
+    @abstractmethod
+    def __updateCondition(self) -> bool:  #implement the update policy based on the Month End Closing activities and the OneStream's load balance 
+        pass
+
+    def __firsUpdate(self) -> None: 
         query = f"SELECT{self.queryParams.columns} \
-             from {self.queryParams.tables} \
-                 where {self.queryParams.conditions} \
-                    and timestamp >= {self.lastUpdate} and  timestamp < {updateTo}" #wrote a good query
-        self.lastUpdate= datetime.today()
+             from {self.queryParams.tables}"
+        self.lastUpdate= time.time()
         data = self.__readFromOneStream(query)
-        self.__updateCopyDatabase(data)
-        #drop da hist data
+        self.__insertHistDatabase(data)
         
     @abstractmethod
-    def __readFromOneStream(self,query:str)-> Data: # use OneStream API to execute the query and return the result represented with our Data format
+    def __readFromOneStream(self, query: str)-> Data: # use OneStream API to execute the query and return the result represented with our Data format
         pass
     
     @abstractmethod
-    def __updateCopyDatabase(self,data :Data) -> None: #execute insertion in historical database
+    def __insertHistDatabase(self, data: Data) -> None: #execute insertion in historical database 
         pass
 
     @abstractmethod
